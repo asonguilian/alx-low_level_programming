f3b28a74f7a0cc2be98cfae42f8992a00282c18a
Guilian Asongtia <asonguilian@gmail.com>
2023-03-16 04:29:26 -0700

    Adds file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main-entry point
+ *prints a message on std output and exits
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is  apiece of art,\n");
+	return (0);
+}
