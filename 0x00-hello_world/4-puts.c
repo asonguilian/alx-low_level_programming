a1091a405919a9f7a9b359b9746c317e907c8660
Guilian Asongtia <asonguilian@gmail.com>
2023-03-16 04:18:26 -0700

    Adds file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main-entry point
+ *prints a message on std output and exits
+ *Return: always 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
