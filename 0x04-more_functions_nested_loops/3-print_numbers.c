7b98a1dde4958fbecdfa84d3a6d913c1f278655e
Guilian Asongtia <asonguilian@gmail.com>
2023-03-23 05:11:36 -0700

    Updates file 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
   *print_numbers - print the numbers from 0 to 9
   *
@@ -7,7 +8,7 @@ void print_numbers(void)
 	int i;
 
 	for (i = 48; i <= 57; i++)
-		_putchar('0' + i);
+		_putchar(i);
 	_putchar('\n');
 }
 
