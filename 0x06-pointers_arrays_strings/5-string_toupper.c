468c8056416011e57c2bfaf3e96e7aa391d60b6f
Guilian Asongtia <asonguilian@gmail.com>
2023-03-30 02:06:23 -0700

    Adds file 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+/**
+  * string_toupper - converts all lowercas letters in string to uppercase
+  *@s: input string
+  * Return: output string
+  */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
