de583231fcb14681ff945ab7bc46c467d77fa918
Guilian Asongtia <asonguilian@gmail.com>
2023-03-30 04:23:41 -0700

    adds file 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+/**
+  * leet - encodes a string into 1337
+  *@s: input string
+  * Return: the pointer to input string that was encoded
+  */
+
+char *leet(char *s)
+{
+	int count = 0, i;
+	int lower[] = {97, 101, 111, 116, 108};
+	int upper[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (s[count] != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (s[count] == lower[i] || s[count] == upper[i])
+			{
+				s[count] = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+	return (s);
+}
