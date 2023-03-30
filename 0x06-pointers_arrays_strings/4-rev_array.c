0269bd05b54a49da0064d7a5d2b5e13bfabc8383
Guilian Asongtia <asonguilian@gmail.com>
2023-03-30 01:42:53 -0700

    Adds file 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+/**
+  * reverse_array - reverses the content of an array of integers
+  *@a: pointer to array
+  *@n: number of elements in the array
+  */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp, mid;
+
+	mid = n / 2;
+	for (i = 0; i <= mid; i++)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + (n - 1 - i));
+		*(a + (n - 1 - i)) = temp;
+	}
+}
