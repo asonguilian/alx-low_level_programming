3477e26874f28bfd6ae9dc304151e429134329fd
Guilian Asongtia <asonguilian@gmail.com>
2023-04-03 04:47:22 -0700

    Adds folder for project even more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+  * _memset - fills the memory with constant byte
+  *@s: pointer to memory area
+  *@b: constant byte 
+  *@n: number of bytes of memory area to be filled
+  *Return - pointer to memory area s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
