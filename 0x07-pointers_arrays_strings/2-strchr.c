7c2550141c270eada4ee716edce46501012df5de
Guilian Asongtia <asonguilian@gmail.com>
2023-04-03 08:03:35 -0700

    Adds file 2.strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+  * _strchr - locates a character in a string
+  * @s: inout string
+  * @c: character to be located
+  * Return:  the pointer to the first occurence of the character c.
+  */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; s[i] != '\0'; i++)
+		if (s[i] == c
+				return (s[i]);
+	if (s[i] == c)
+		return (s[i]);
+	return ('\0');
+}
+
