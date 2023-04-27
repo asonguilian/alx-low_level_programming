6893211076e5033f0ea8f4ecceaa3f050b6fd957
Guilian Asongtia <asonguillian@gmail.com>
2023-04-27 13:58:27 +0000

    Asm code now works perfectly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,15 @@
 	SECTION .data
 message:	db "Hello, Holberton", 0
-format:	db "%s", 10, 0
+format:		db "%s", 10, 0
 
 	SECTION .text
 	extern printf
 	global main
 main:
 	mov esi, message
-	move edi, format
-	move eax, 0
+	mov edi, format
+	mov eax, 0
 	call printf
 
-	move eax, 0
+	mov eax, 0
 	ret
