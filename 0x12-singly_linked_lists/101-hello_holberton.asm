b2c638addb2ca1e92347508fc9fde49d1642573e
Guilian Asongtia <asonguillian@gmail.com>
2023-04-27 13:38:43 +0000

    Updates asm file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,44 +1,16 @@
-	.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, holberton"
-	.text
-	.globl	main
-	.type	main, @function
+section .data
+	hello db "Hello, Holberton", 0
+	format db "%s\n", 0
+
+section .text
+	global main
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
+	move rdi, format
+	move rsi, hello
+	;call printf function
+	xor eax, eax
+	call printf
+	;exit
+	xor eax, eax
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
